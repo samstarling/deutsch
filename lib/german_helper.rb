fc24ed8b6560cd3901f6b4a30681cb2ea4b33fa5
Sam Starling <mail@samstarling.co.uk>
2016-03-15 22:18:21 +0100

    A whole load of refactoring

--- lib/german_helper.rb
+++ lib/german_helper.rb
@@ -1,34 +1,85 @@
-module GermanHelper
-  def nom base_article, word
-    case_tag 'nom', base_article, base_article, word
+class Word
+  def initialize gender, word
+    @gender = gender
+    @word = word
   end
 
-  def akk base_article, word
-    mapping = {
-      'der' => 'den'
+  def definite_article
+    DefiniteArticle.for(self, :n)
+  end
+
+  def gender
+    @gender
+  end
+
+  def word
+    @word
+  end
+end
+
+class DefiniteArticle
+  def self.for w, c
+    rules = {
+      m:  { n: 'der', a: 'den', d: 'dem' },
+      f:  { n: 'die', a: 'die', d: 'der' },
+      n:  { n: 'das', a: 'das', d: 'dem' },
+      pl: { n: 'die', a: 'die', d: 'den' }
     }
-    article = mapping[base_article] || base_article
-    case_tag 'akk', base_article, article, word
+    rules[w.gender][c]
   end
+end
 
-  def dat base_article, word
-    mapping = {
-      'der' => 'dem',
-      'die' => 'der',
-      'das' => 'dem',
-      'die plural' => 'den',
+class IndefiniteArticle
+  def self.for w, c
+    rules = {
+      m:  { n: 'ein',  a: 'einen', d: 'einem' },
+      f:  { n: 'eine', a: 'eine',  d: 'einer' },
+      n:  { n: 'ein',  a: 'ein',   d: 'einem' }
     }
-    article = mapping[base_article] || base_article
-    case_tag 'dat', base_article, article, word
+    rules[w.gender][c]
+  end
+end
+
+class WordDisplay
+  def initialize prefix, w, c
+    @prefix = prefix
+    @word = w
+    @case = c
   end
 
-  private
-
-  def case_tag type, base_article, article, word
-    if base_article == article
-      "<span class='case #{type}'>#{article} #{word}</span>"
+  def to_s
+    if @prefix == @word.definite_article
+      "<span class='case-#{@case}'>#{@prefix} #{@word.word}</span>"
     else
-      "<span class='case #{type}'><abbr title='#{type}: #{base_article} &rarr; #{article}'>#{article}</abbr> #{word}</span>"
+      "<span class='case-#{@case}'><abbr title='#{@word.definite_article}'>#{@prefix}</abbr> #{@word.word}</span>"
     end
+  end
+end
+
+module GermanHelper
+  def definite_article w, c
+    prefix = DefiniteArticle.for(w, c)
+    WordDisplay.new(prefix, w, c)
+  end
+
+  def indefinite_article w, c
+    prefix = IndefiniteArticle.for(w, c)
+    WordDisplay.new(prefix, w, c)
+  end
+
+  def der word
+    Word.new(:m, word)
+  end
+
+  def die word
+    Word.new(:f, word)
+  end
+
+  def das word
+    Word.new(:n, word)
+  end
+
+  def pl word
+    Word.new(:pl, word)
   end
 end
